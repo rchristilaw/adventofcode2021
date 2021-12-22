c5eda1238ab387abd1fdb61bbacc73c6602dba22
Ryan Christilaw <ryan.christilaw@gmail.com>
2021-12-22 09:04:30 -0500

    Day 21 Part 2

--- src/main/java/Day21.java
+++ src/main/java/Day21.java
@@ -1,9 +1,11 @@
 import lombok.AllArgsConstructor;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 public class Day21 extends BaseDay {
     private final static Logger log = Logger.getLogger(Day21.class.getName());
@@ -12,11 +14,13 @@ public class Day21 extends BaseDay {
     private static final int PLAYER_1_START = 7;
     private static final int PLAYER_2_START = 8;
 
+    private Map<String, GameResult> cache;
+
     public static void main(String... args) throws URISyntaxException, IOException {
         final var day = new Day21();
 
         day.runPart1();
-//        day.runPart2();
+        day.runPart2();
     }
 
     public void runPart1() throws URISyntaxException, IOException {
@@ -29,7 +33,7 @@ public class Day21 extends BaseDay {
 
         int winningScore = 0;
         int losingScore = 0;
-        while(winningScore == 0) {
+        while (winningScore == 0) {
 
             dice = roll(player1, dice);
 
@@ -54,7 +58,7 @@ public class Day21 extends BaseDay {
 
     private int roll(Player player, int dice) {
         int totalMove = 0;
-        for (int i = 1; i<=3; i++) {
+        for (int i = 1; i <= 3; i++) {
             player.rollCount++;
             dice++;
             if (dice > 100) {
@@ -76,10 +80,82 @@ public class Day21 extends BaseDay {
     }
 
     public void runPart2() throws URISyntaxException, IOException {
-        final var input = readClassPathResource(inputFile).lines().collect(Collectors.toList());
+//        final var input = readClassPathResource(inputFile).lines().collect(Collectors.toList());
 
-        var total = 0;
-        log.info("TOTAL: " + total);
+        cache = new HashMap<>();
+        var result = recursiveRoll(0, PLAYER_1_START, 0, PLAYER_2_START, true, 0, 0);
+
+        log.info("Part 2 Total: \n" + result.winsPlayer1 + "\n" + result.winsPlayer2);
+
+    }
+
+    private GameResult recursiveRoll(int p1Score, int p1Pos, int p2Score, int p2Pos, boolean p1Turn, int rollCount, int roll) {
+        var result = new GameResult();
+
+
+        if (p1Turn) {
+            p1Pos = movePlayer(roll, p1Pos);
+        } else {
+            p2Pos = movePlayer(roll, p2Pos);
+        }
+
+        if (rollCount == 3) {
+            rollCount = 0;
+
+
+            if (p1Turn) {
+                p1Score += p1Pos;
+                if (p1Score >= 21) {
+                    result.winsPlayer1 = BigDecimal.ONE;
+                    return result;
+                }
+            } else {
+                p2Score += p2Pos;
+                if (p2Score >= 21) {
+                    result.winsPlayer2 = BigDecimal.ONE;
+                    return result;
+                }
+            }
+
+            p1Turn = !p1Turn;
+        }
+
+        rollCount++;
+
+        for (int r = 1; r <= 3; r++) {
+            GameResult subResult;
+
+            var key = p1Score + "_" + p1Pos + "_" + p2Score + "_" + p2Pos + "_" + p1Turn + "_" + rollCount+ "_" + r;
+
+            if (cache.get(key) != null) {
+                subResult = cache.get(key);
+            } else {
+                subResult = recursiveRoll(p1Score, p1Pos, p2Score, p2Pos, p1Turn, rollCount, r);
+                cache.put(key, subResult);
+            }
+            result.addResult(subResult);
+        }
+        return result;
+    }
+
+    private int movePlayer(int diceRoll, int position) {
+        for (int i = 1; i <= diceRoll; i++) {
+            position++;
+            if (position > 10) {
+                position = 1;
+            }
+        }
+        return position;
+    }
+
+    private static class GameResult {
+        private BigDecimal winsPlayer1 = BigDecimal.ZERO;
+        private BigDecimal winsPlayer2 = BigDecimal.ZERO;
+
+        void addResult(GameResult result) {
+            winsPlayer1 = winsPlayer1.add(result.winsPlayer1);
+            winsPlayer2 = winsPlayer2.add(result.winsPlayer2);
+        }
     }
 
     @AllArgsConstructor
